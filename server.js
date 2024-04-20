d969307bc70bc70f486bdf8059906ccc6673c8ad
aishtomer <aishwaryatomer@gmail.com>
2024-04-20 06:55:33 +0100

    Initialize the app and set the connection with the database.

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+    'taskmanager7860',
+    'aishtomer',
+    'Ast@8874635984',
+    {
+        host: 'db4free.net',
+        dialect: 'mysql'
+    }
+);
+
+sequelize.authenticate().then(() => {
+   console.log('Connection has been established successfully.');
+}).catch((error) => {
+   console.error('Unable to connect to the database: ', error);
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'very-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
